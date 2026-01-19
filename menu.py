b80b41da5ca1b5ebb8979782c3535a8064c9c78c
GreGrenier <GreGrenier@users.noreply.github.com>
2026-01-19 21:34:38 +0100

    click fonctionnality

--- menu.py
+++ menu.py
@@ -7,10 +7,13 @@
 
 from game import *
 import pygame
+import os
 
 def init() :
     global font
-    global text 
+    global text
+    global button_cooldown
+    button_cooldown = 0 
 
     pygame.init()
     text = "_"
@@ -18,24 +21,38 @@ def init() :
 
 def update(events, screen) :
     global text
+    global button_cooldown
 
     width, height = screen.get_size() 
 
     # Header
     pygame.draw.rect(screen, (255, 201, 157), (0, 0, width, 720), width=0)
 
+    while button_cooldown > 0:
+        button_cooldown -= 1
+        pygame.time.wait(1)
 
-    createButton("assets/play_button_nor.png", "assets/play_button_mouse.png", "assets/play_button_click.png",screen, (width/2, height/2), 48*4, 24*4)
+    # Play button
+    if createButton(os.sep.join(["assets", "play_button_nor.png"]), os.sep.join(["assets", "play_button_mouse.png"]), os.sep.join(["assets", "play_button_click.png"]), screen, (width/2, height/2), 48*4, 24*4, events) == True:
+        pass
 
-def createButton(image_nor, image_mouse, image_click, screen, position, width, height):
+def createButton(image_nor, image_mouse, image_click, screen, position, width, height, events):
+    global button_cooldown
+    click = False
     image = pygame.image.load(image_nor)
     scaled_image = pygame.transform.scale(image, (width, height))
     rect = scaled_image.get_rect()
     rect.center = position
     if rect.collidepoint(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1]):
         image = pygame.image.load(image_mouse)
-        scaled_image = pygame.transform.scale(image, (width, height))
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONUP:
+                button_cooldown = 80
+                click = True
+                image = pygame.image.load(image_click)
+    scaled_image = pygame.transform.scale(image, (width, height))
     screen.blit(scaled_image, rect)
+    return click
     
 
 menuView = view(init, update)
