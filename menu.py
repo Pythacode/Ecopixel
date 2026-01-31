949888f93bcadeaebb058537b6b5ee722a186f90
Nathanaëlle <titouan.petit-frulio@laposte.net>
2026-01-31 20:44:53 +0100

    replace init by __init__

--- menu.py
+++ menu.py
@@ -14,17 +14,7 @@ from gameView import gameView
 
 class menuView():
 
-    def PlayButton_Pressed(self):
-        main_game.change_view(gameView)
-
-    def QuitButton_Pressed(self):
-        main_game.running = False 
-
-    def Logo_Pressed(self):
-        babysfx = pygame.mixer.Sound(os.sep.join(["assets", "sfx", "BabyNoise.mp3"]))
-        babysfx.play()
-
-    def init(self) :
+    def __init__(self) :
 
         pygame.init()
         self.font = pygame.font.Font("freesansbold.ttf", 24)
@@ -41,6 +31,15 @@ class menuView():
         self.quitbutton = button(os.sep.join(["assets", "image", "button", "button_nor.png"]), os.sep.join(["assets", "image", "button", "button_mouse.png"]), os.sep.join(["assets", "image", "button", "button_click.png"]), (640, 460), 48*4, 24*4, self.QuitButton_Pressed, text="Quit")
         self.quitbutton.createButton()
 
+    def PlayButton_Pressed(self):
+        main_game.change_view(gameView)
+
+    def QuitButton_Pressed(self):
+        main_game.running = False 
+
+    def Logo_Pressed(self):
+        babysfx = pygame.mixer.Sound(os.sep.join(["assets", "sfx", "BabyNoise.mp3"]))
+        babysfx.play()
 
     def update(self, events) :
 
