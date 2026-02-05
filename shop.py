17b2b0431002b24d4eb715cd245d86affc9b87e0
PtitBillyboy <vpierrick1@gmail.com>
2026-02-05 21:42:03 +0100

    Shop

--- shop.py
+++ shop.py
@@ -1,7 +1,12 @@
+import os
+import json
+from game import*
+
 class shopView():
-    def  init (self):
-        self.item
-        self.background
+    def __init__ (self):
+        tilsetJsonFile = open(os.sep.join([main_game.asset_doc,"item.Json"]))
+        self.items = json.load(tilsetJsonFile)
+
 
     def  update (self,event):
         main_game.screen.fill("#ffe75d")
