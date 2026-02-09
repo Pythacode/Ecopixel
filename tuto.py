824686c12cd691dd93a56bc52c0522f55632b35d
Nathanaëlle <titouan.petit-frulio@laposte.net>
2026-02-09 13:59:38 +0100

    Start tuto

---
+++ tuto.py
@@ -0,0 +1,11 @@
+# -------------------------------- tuto -------------------------------- #
+# Lien du dépot : http://github.com/Pythacode/Ecopixel                   #
+# Fichier contenant le tuto du jeux                                      #
+# Crée par Titouan - https://github.com/Pythacode/                       #
+# License : Creative Commons Attribution-NonCommercial 4.0 International #
+# ---------------------------------------------------------------------- #
+
+class Tuto() :
+    def __init__(self):
+        self.step = ["search", "plant", "buy"]
+        self.avancement = 0
