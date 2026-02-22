360e7dfbe6342abb348b72b9efc049692cc93081
PtitBillyboy <vpierrick1@gmail.com>
2026-02-22 19:09:22 +0100

    J'ai rajouter pas mal de truc dans le shop

--- sources/shop.py
+++ sources/shop.py
@@ -12,15 +12,17 @@ from game import*
 class shopView():
     def __init__ (self):
         tilsetJsonFile = open(os.sep.join([main_game.asset_doc, "image", "item", "items.json"]))
-        self.image_arrosoir,self.rect_arrosoir = self.img(288,288, (200, 150),"arrosoir.png")
-        self.image_engrais,self.rect_engrais = self.img(192,192,(450,150),"engrais.png")
-        self.image_pot,self.rect_pot = self.img(192,192,(700,150),"pot.png")
-        self.image_ruche,self.rect_ruche = self.img(192,192,(950,150),"ruche.png")
+        self.image_house2,self.rect_house2 = self.img(288,288, (200,450),"houses","house2.png")
+        self.image_house3,self.rect_house3 = self.img(288,288, (500,450),"houses","house3.png")
+        self.image_house4,self.rect_house4 = self.img(288,288, (800,450),"houses","house4.png")
+        self.image_rupture,self.rect_rupture = self.img(288,288, (200,150),"shop","Rupture.png")
+        self.image_arrosoir,self.rect_arrosoir = self.img(288,288, (200, 150),"item","arrosoir.png")
+        self.image_engrais,self.rect_engrais = self.img(192,192,(450,150),"item","engrais.png")
+
         self.arrosoir_btn = button(os.sep.join([main_game.asset_doc, "image", "button", "button_nor.png"]), os.sep.join([main_game.asset_doc, "image", "button", "button_mouse.png"]), os.sep.join([main_game.asset_doc, "image", "button", "button_click.png"]), (180,300), 192, 192/2, self.Fonction_quand_on_clique,"Arrosoir")
         self.engrais_btn = button(os.sep.join([main_game.asset_doc, "image", "button", "button_nor.png"]), os.sep.join([main_game.asset_doc, "image", "button", "button_mouse.png"]), os.sep.join([main_game.asset_doc, "image", "button", "button_click.png"]), (450,300), 192, 192/2, self.Fonction_quand_on_clique,"Engrais")
-        self.pot_btn = button(os.sep.join([main_game.asset_doc, "image", "button", "button_nor.png"]), os.sep.join([main_game.asset_doc, "image", "button", "button_mouse.png"]), os.sep.join([main_game.asset_doc, "image", "button", "button_click.png"]), (700,300), 192, 192/2, self.Fonction_quand_on_clique,"Pot")
-        self.ruche_btn = button(os.sep.join([main_game.asset_doc, "image", "button", "button_nor.png"]), os.sep.join([main_game.asset_doc, "image", "button", "button_mouse.png"]), os.sep.join([main_game.asset_doc, "image", "button", "button_click.png"]), (950,300), 192, 192/2, self.Fonction_quand_on_clique,"Ruche")
-
+        self.vendre_btn = button(os.sep.join([main_game.asset_doc, "image", "button", "button_nor.png"]), os.sep.join([main_game.asset_doc, "image", "button", "button_mouse.png"]), os.sep.join([main_game.asset_doc, "image", "button", "button_click.png"]), (1160,75), 192+25, 192/3, self.Fonction_quand_on_clique,"Vendre")
+        self.upgrade_btn = button(os.sep.join([main_game.asset_doc, "image", "button", "button_nor.png"]), os.sep.join([main_game.asset_doc, "image", "button", "button_mouse.png"]), os.sep.join([main_game.asset_doc, "image", "button", "button_click.png"]), (180,650), 192, 192/2, self.Amelioration,"Ameliorer la Maison")
 
         self.items = json.load(tilsetJsonFile)
         self.header = True
@@ -31,14 +33,13 @@ class shopView():
         main_game.screen.fill("#ffe75d")
         main_game.screen.blit(self.image_arrosoir,self.rect_arrosoir)
         main_game.screen.blit(self.image_engrais,self.rect_engrais)
-        main_game.screen.blit(self.image_pot,self.rect_pot)
-        main_game.screen.blit(self.image_ruche,self.rect_ruche)
+        main_game.screen.blit(self.image_house2,self.rect_house2)
         self.arrosoir_btn.update(main_game.screen)
         self.engrais_btn.update(main_game.screen)
-        self.pot_btn.update(main_game.screen)
-        self.ruche_btn.update(main_game.screen)
+        self.vendre_btn.update(main_game.screen)
+        self.upgrade_btn.update(main_game.screen)
 
-    def img(self,width,height,position,path):
+    def img(self,width,height,position,dossier,png):
         """
         Données pour la fonction img:
 
@@ -50,11 +51,16 @@ class shopView():
         """
         # Résultat : Affiche une image sur la fenêtre
 
-        image = pygame.image.load(os.sep.join([main_game.asset_doc, "image", "item", path]))
+        image = pygame.image.load(os.sep.join([main_game.asset_doc, "image", dossier, png]))
         image = pygame.transform.scale(image, (width,height))
         rect = image.get_rect()
         rect.center = position
         return image, rect
     
     def Fonction_quand_on_clique(self):
+        main_game.screen.blit(self.image_rupture,self.rect_rupture)
+        
+
+    def Amelioration(self):
+
         print("test")
