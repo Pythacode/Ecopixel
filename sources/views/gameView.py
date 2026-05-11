96aaffda0ab5babaa31ddf2307a6f3191ee370db
GreGrenier <journetlucas777@gmail.com>
2026-05-11 23:18:51 +0200

    tree growing

--- sources/views/gameView.py
+++ sources/views/gameView.py
@@ -327,7 +327,7 @@ class gameView() :
                     case 'pos' :
                         self.players[data['username']].x = data['pos']
                     case 'new_tree':
-                        continue # Ca fait beuger tous le jeu 😭😭
+                        print(data)
                         for tree in self.trees:
                             if tree.x == data["x"]:
                                 tree.skin_index = data["skin_index"]
